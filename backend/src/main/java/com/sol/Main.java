b185357cdfb3a0a3e84d59107da85d7db1c2d285
Naolenja <dan.tsvetaev@outlook.com>
2024-10-17 16:27:38 +0300

    removed unused import

--- backend/src/main/java/com/sol/Main.java
+++ backend/src/main/java/com/sol/Main.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import java.util.List;
-import java.util.Locale;
 import java.util.Random;
 
 
@@ -17,7 +16,6 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Main.class, args);
     }
     @Bean
