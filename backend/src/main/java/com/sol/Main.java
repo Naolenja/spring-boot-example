110eb00ca44cbc72d3dfd30cef4f3bd758e2374a
Naolenja <dan.tsvetaev@outlook.com>
2024-10-19 17:51:28 +0300

    test

--- backend/src/main/java/com/sol/Main.java
+++ backend/src/main/java/com/sol/Main.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import java.util.List;
 import java.util.Random;
+
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
